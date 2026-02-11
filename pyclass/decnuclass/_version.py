54c06285c3a34a16b07251f123dd83cbcda4340c
Willem Elbers <whe@willemelbers.com>
2026-02-11 17:22:06 +0000

    Load more recent version of class_decnu

--- pyclass/decnuclass/_version.py
+++ pyclass/decnuclass/_version.py
@@ -1,2 +1,2 @@
 class_version = '3.2.2'
-url = 'https://github.com/wullm/class_decnu/archive/d5693a3.tar.gz'.format(class_version)
+url = 'https://github.com/wullm/class_decnu/archive/1f0ca79.tar.gz'.format(class_version)
