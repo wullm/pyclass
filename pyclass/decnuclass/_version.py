87c290633cf78c9181720693a7d8e8ed80e7059a
Willem Elbers <whe@willemelbers.com>
2026-02-11 19:27:58 +0000

    Update to the latest version of class_decnu

--- pyclass/decnuclass/_version.py
+++ pyclass/decnuclass/_version.py
@@ -1,2 +1,2 @@
 class_version = '3.2.2'
-url = 'https://github.com/wullm/class_decnu/archive/1f0ca79.tar.gz'.format(class_version)
+url = 'https://github.com/wullm/class_decnu/archive/d3da6e2.tar.gz'.format(class_version)
